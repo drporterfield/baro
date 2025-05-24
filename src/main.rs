6549430b73c104b29476848564b827c6b1251622
John Porterfield <jporterfield@bridgesourcemedical.com>
2025-05-24 14:47:51 -0500

    accurately detects austin elevation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
+//! Reads the Barometric Pressure
+
 #![no_std]
 #![no_main]
 
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_time::Timer;
+use embassy_nrf::twim::{self, Twim};
+use embassy_nrf::{bind_interrupts, peripherals};
+use embassy_time::Delay;
 use {defmt_rtt as _, panic_probe as _};
+use bmp390::{Bmp390};
+
+bind_interrupts!(struct Irqs {
+    TWISPI0 => twim::InterruptHandler<peripherals::TWISPI0>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let mut led1 = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
-    let mut led2 = Output::new(p.P0_14, Level::Low, OutputDrive::Standard);
+    info!("Initializing TWI...");
+    let twim_config = twim::Config::default();
+    let twi = Twim::new(
+        p.TWISPI0, 
+        Irqs, 
+        p.P0_27, // SDA
+        p.P0_28, // SCL
+        twim_config);
 
-    loop {
-        led1.set_high();
-        led2.set_low();
-        Timer::after_millis(300).await;
-        led1.set_low();
-        led2.set_high();
-        Timer::after_millis(300).await;
-    }
+    info!("Initializing BMP...");
+    let bmp390_config = bmp390::Configuration::default();
+    let mut sensor = Bmp390::try_new(twi, bmp390::Address::Up, Delay, &bmp390_config)
+        .await
+        .unwrap();
+
+    // take one measurement and print it to info
+    let measurement = sensor.measure().await.unwrap();
+    defmt::info!("Measurement: {}", measurement);
 }
